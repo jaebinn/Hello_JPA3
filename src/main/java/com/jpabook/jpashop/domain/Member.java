723618951a1d520ed88a3e370304be1158db6138
jaebinn <whiteclover129@gmail.com>
2024-07-29 16:27:59 +0900

    회원 등록 수정 조회 API

--- src/main/java/com/jpabook/jpashop/domain/Member.java
+++ src/main/java/com/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package com.jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,9 +17,11 @@ public class Member {
     @Column(name="member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
+    @JsonIgnore
     private Address address;
 
     @OneToMany(mappedBy = "member")
